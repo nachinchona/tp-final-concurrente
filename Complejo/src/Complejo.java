980b8ec34c214a152ccbb744e014e46090a9df2c
nachinchona <ignacio.navarro@est.fi.uncoma.edu.ar>
2024-07-24 20:29:18 -0300

    init

---
+++ Complejo/src/Complejo.java
@@ -0,0 +1,7 @@
+public class Complejo {
+    public static void main(String[] args) throws Exception {
+        System.out.println("------------------------------------");
+        System.out.println("Hello, World!");
+        System.out.println("------------------------------------");
+    }
+}
